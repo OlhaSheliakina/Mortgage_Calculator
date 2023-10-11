cd8ed6145e0e2c0be7c3354859914c1e36d64725
Olha Sheliakina <olya.shelyakina@gmail.com>
2023-10-12 00:59:54 +0300

    adding dark theme

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
+import CssBaseline from '@mui/material/CssBaseline';
+import { ThemeProvider } from '@mui/material/styles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+      <CssBaseline />
+    </ThemeProvider>
   </React.StrictMode>
 );
